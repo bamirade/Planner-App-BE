63b575638e6dc783d1c31a717953206a47568367
bamirade <118993260+bamirade@users.noreply.github.com>
2023-07-13 20:18:01 +0800

    Initial

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :tasks
+  resources :categories
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :categories do
+    resources :tasks
+  end
+
+end
