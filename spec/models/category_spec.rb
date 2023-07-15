99df99b7e860097c319b71e67719cd8da4a98c4a
bamirade <118993260+bamirade@users.noreply.github.com>
2023-07-15 10:48:57 +0800

    Model Test Update
    +added FactoryBot and Faker usage

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe 'validations' do
     it 'validates the presence of name' do
-      category = Category.new(name: nil)
+      category = FactoryBot.build(:category, name: nil)
       expect(category).not_to be_valid
       expect(category.errors[:name]).to include("can't be blank")
     end
@@ -11,12 +11,9 @@ RSpec.describe Category, type: :model do
 
   describe 'associations' do
     it 'has many tasks' do
-      category = Category.new(name: 'Work')
-      expect(category.tasks).to be_empty
-
-      task1 = category.tasks.build(name: 'Task 1')
-      task2 = category.tasks.build(name: 'Task 2')
-
+      category = FactoryBot.create(:category)
+      task1 = FactoryBot.create(:task, category: category)
+      task2 = FactoryBot.create(:task, category: category)
       expect(category.tasks).to include(task1, task2)
     end
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validations' do
     it 'validates the presence of name' do
-      task = Task.new(name: nil, description: 'Task description', due_date: Date.today, category_id: 1)
+      task = FactoryBot.build(:task, name: nil)
       expect(task).not_to be_valid
       expect(task.errors[:name]).to include("can't be blank")
     end
 
     it 'validates the presence of description' do
-      task = Task.new(name: 'Task name', description: nil, due_date: Date.today, category_id: 1)
+      task = FactoryBot.build(:task, description: nil)
       expect(task).not_to be_valid
       expect(task.errors[:description]).to include("can't be blank")
     end
 
     it 'validates the presence of due_date' do
-      task = Task.new(name: 'Task name', description: 'Task description', due_date: nil, category_id: 1)
+      task = FactoryBot.build(:task, due_date: nil)
       expect(task).not_to be_valid
       expect(task.errors[:due_date]).to include("can't be blank")
     end
@@ -23,9 +23,8 @@ RSpec.describe Task, type: :model do
 
   describe 'associations' do
     it 'belongs to a category' do
-      category = Category.create(name: 'Work')
-      task = category.tasks.build(name: 'Task 1', description: 'Task description', due_date: Date.today)
-
+      category = FactoryBot.create(:category)
+      task = FactoryBot.create(:task, category: category)
       expect(task.category).to eq(category)
     end
   end
