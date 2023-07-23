d2dc8ca8b96bc968fc87dcb61943779ac5b3965b
bamirade <118993260+bamirade@users.noreply.github.com>
2023-07-24 06:36:48 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_210515) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.date "due_date"
+    t.datetime "due_date"
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
