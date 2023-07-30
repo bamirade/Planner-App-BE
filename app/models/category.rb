cdc1abd7040fb1094eb30b407b2812a2aea6cc19
bamirade <118993260+bamirade@users.noreply.github.com>
2023-07-31 05:46:43 +0800

    Model Adjustment

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
