3bdcad087344cc6cef69589cd72ec9bedd6b43ac
bamirade <118993260+bamirade@users.noreply.github.com>
2023-07-31 06:17:42 +0800

    Token Expiry

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,13 +1,21 @@
 module AuthHelper
   SECRET_KEY = ENV['SECRET_KEY_BASE']
 
+  EXPIRATION_TIME = 168.hours.to_i
+
   def self.generate_token(user_id)
-    payload = { user_id: user_id }
+    expiration_time = Time.now.to_i + EXPIRATION_TIME
+    payload = { user_id: user_id, exp: expiration_time }
     JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
   def self.decode_token(token)
-    JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256').first
+    decoded_token = JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256').first
+    if decoded_token['exp'] && Time.now.to_i < decoded_token['exp']
+      decoded_token
+    else
+      nil
+    end
   rescue JWT::ExpiredSignature
     nil
   end
