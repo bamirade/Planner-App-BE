d9d1593acdc7cff744910e947b0de37549a4c57e
bamirade <118993260+bamirade@users.noreply.github.com>
2023-09-21 23:08:53 +0800

    CI Pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_26_201359) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
@@ -26,9 +23,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_201359) do
     t.string "name"
     t.text "description"
     t.datetime "due_date"
-    t.bigint "category_id", null: false
+    t.integer "category_id", null: false
     t.boolean "is_completed", default: false, null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
